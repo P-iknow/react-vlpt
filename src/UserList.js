06d96dfe595e74f22596130e0c86b8f43e0fd9f6
P-iknow <apricotsoul@gmail.com>
2019-08-15 14:35:00 +0900

    feat: React.memo 를 사용한 컴포넌트 리렌더링 방지

---
+++ src/UserList.js
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+
+const User = ({user, onRemove, onToggle}) => {
+  // useEffect( () => {
+  //   console.log(user);
+  // });
+  return (
+    <div>
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active? 'green' : 'black'
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
+    </div>
+  );
+}
+
+const UserList = ({users, onRemove, onToggle}) => {
+  return (
+    <div>
+      {users.map(user => (
+        <User user={user} onRemove={onRemove} onToggle={onToggle} key={user.id} />
+      ))}
+    </div>
+  )
+}
+
+export default React.memo(UserList);
