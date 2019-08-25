8c319d342d13b91fd936e737ddb5266d101a5800
P-iknow <apricotsoul@gmail.com>
2019-08-25 10:28:47 +0900

    feat: 가계부 연습 완성

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 
-
+const isNameSlashPrice = (str) => {
+  return /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|\*]+$/.test(str)
+} 
 
 const Form = ({ items, setItems }) => {
   const [value, setValue] = useState("")
@@ -9,6 +11,10 @@ const Form = ({ items, setItems }) => {
   }
 
   const onCreate = () => {
+    if(!isNameSlashPrice(value)) {
+      alert("name/price ex) banana/1000 형식으로 입력하세요")
+      return null;
+    }
     const [name, strPrice] = value.split("/");
     const price = Number(strPrice);
     const id = Date.now();
@@ -18,7 +24,7 @@ const Form = ({ items, setItems }) => {
 
   return (
     <div className="form">
-      <input value={value} onChange={onChange}/>
+      <input value={value} onChange={onChange} placeHolder="name/price"/>
       <button className="create-button" onClick={onCreate}>
         Add
       </button>
