ad813478158a630bc32dccc3d62b35d87fde451b
P-iknow <apricotsoul@gmail.com>
2019-08-30 18:37:33 +0900

    feat: TodoList Compoents

---
+++ src/03_Todo/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import TodoTemplate from './TodoTemplate';
+import TodoHead from './TodoHead';
+import TodoList from './TodoList';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background: #e9ecef;
+  }
+`
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <TodoTemplate>
+        <TodoHead />
+        <TodoList />
+      </TodoTemplate>
+    </>
+  )
+}
+
+export default App;
