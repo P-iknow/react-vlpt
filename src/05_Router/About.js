9a6832d849c887e112a25e588b167c12fae9588d
P-iknow <apricotsoul@gmail.com>
2019-09-10 18:31:28 +0900

    learn: 05-02 리엑트 라우터-파라미터와 쿼리

--- src/05_Router/About.js
+++ src/05_Router/About.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import qs from 'qs';
 
-const About = () => {
+const About = ({ location }) => {
+  const query = qs.parse(location.search, {
+    ignoreQueryPrefix: true
+  });
+
+  // 쿼리 파싱의 결과값은 문자열 
+  const detail = query.detail === 'true';
+
   return (
     <div>
       <h1>소개</h1>
       <p>이 프로젝트는 리엑트 라우터 기초를 실습해보는 예제 프로젝트  입니다.</p>
+      {detail && <p>추가적인 정보가 어쩌고 저쩌고..</p>}
     </div>
   )
 }
--- src/05_Router/App.js
+++ src/05_Router/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router , Route, Link} from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Profile from './Profile';
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
         <hr />
         <Route path="/" exact={true} component={Home} />
         <Route path="/about" component={About} />
+        <Route path="/profiles/:username" component={Profile} />
       </div>
     </Router>
   )
