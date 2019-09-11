f3897e2aa1ca172e3ca7c60cce14516b4a3fd7d5
P-iknow <apricotsoul@gmail.com>
2019-09-11 16:45:10 +0900

    learn: 04-01 API 연동의 기본

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './05_Router/App'
+import App from './04_API/App'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
