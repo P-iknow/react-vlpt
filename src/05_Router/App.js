b607a2df3390f729101610413e0887eb7dd5afa5
P-iknow <apricotsoul@gmail.com>
2019-09-11 13:42:59 +0900

    learn: 05-04 리엑트 라우터 부가기능

--- src/05_Router/App.js
+++ src/05_Router/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router , Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router,  Switch , Route, Link} from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Profiles from './Profiles';
+import HistorySample from './HistorySmaple';
 
 const App = () => {
   return (
@@ -18,11 +19,26 @@ const App = () => {
           <li>
             <Link to="/profiles">프로필 목록</Link>
           </li>
+          <li>
+            <Link to="/history">예제</Link>
+          </li>
         </ul>
         <hr />
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/profiles" component={Profiles} />
+        <Switch>
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/profiles" component={Profiles} />
+          <Route path='/history' component={HistorySample} />
+          <Route
+          // path 를 따로 정의하지 않으면 모든 상황에 렌더링됨
+          render={({ location }) => (
+            <div>
+              <h2>이 페이지는 존재하지 않습니다:</h2>
+              <p>{location.pathname}</p>
+            </div>
+          )}
+        />
+        </Switch>
       </div>
     </Router>
   )
