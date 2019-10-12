a332eff305c026e1735a4ff79d611b6287f0e04a
P-iknow <apricotsoul@gmail.com>
2019-10-12 17:54:55 +0900

    Learn: 6-8 리덕스 useSelector 최적화

--- src/06_Redux/containers/CounterContainers.js
+++ src/06_Redux/containers/CounterContainers.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import Counter from '../components/Counter';
 import { increase, decrease, setDiff } from '../modules/counter';
 
 const CounterContainers = () => {
   // useSelector는 리덕스 스토어의 상태를 조회하는 Hook이다.
   // state의 값은 store.getState() 를 호출했을 때 나타나는 결과물과 동일하다.
-  const { number, diff } = useSelector(state => ({
-    number: state.counter.number,
-    diff: state.counter.diff,
-  }));
+  const { number, diff } = useSelector(
+    state => ({
+      number: state.counter.number,
+      diff: state.counter.diff,
+    }),
+    shallowEqual
+  );
 
   //useDispatch는 리덕스 스토어의 dispatch를 함수에서 사용할 수 있게 해주는 Hook이다.
   const dispatch = useDispatch();
