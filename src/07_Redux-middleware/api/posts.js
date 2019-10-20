f3a0362b3ef4bdb30a07fa21e802a1f4f4942c89
P-iknow <apricotsoul@gmail.com>
2019-10-20 17:41:08 +0900

    learn: 7-8 json-server

--- src/07_Redux-middleware/api/posts.js
+++ src/07_Redux-middleware/api/posts.js
@@ -1,3 +1,15 @@
+import axios from 'axios';
+
+export const getPosts = async () => {
+  const response = await axios.get('http://localhost:4000/posts');
+  return response.data;
+};
+
+export const getPostById = async id => {
+  const response = await axios.get(`http://localhost:4000/posts/${id}`);
+  return response.data;
+};
+
 // n 밀리세컨드 동안 기다리는 프로미스를 만들어주는 함수
 
 const sleep = n => new Promise(resolve => setTimeout(resolve, n));
@@ -22,14 +34,14 @@ const posts = [
   },
 ];
 
-// 포스트 목록을 가져오는 비동기 함수
-export const getPosts = async () => {
-  await sleep(500); // 0.5 초 쉬고
-  return posts; // post 배열
-};
+// // 포스트 목록을 가져오는 실험용 비동기 함수
+// export const getPosts = async () => {
+//   await sleep(500); // 0.5 초 쉬고
+//   return posts; // post 배열
+// };
 
-// ID 로 포스트를 조회하는 비동기 함수
-export const getPostById = async id => {
-  await sleep(500);
-  return posts.find(post => post.id === id); //id로 찾아서 반환
-};
+// // ID 로 포스트를 조회하는 비동기 함수
+// export const getPostById = async id => {
+//   await sleep(500);
+//   return posts.find(post => post.id === id); //id로 찾아서 반환
+// };
